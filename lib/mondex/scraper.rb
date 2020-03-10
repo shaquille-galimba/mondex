57cda1ee319dce3ebd7e44d80140440c2b07d0b7
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-10 08:32:54 +0000

    Updated scraper

--- lib/mondex/scraper.rb
+++ lib/mondex/scraper.rb
@@ -2,22 +2,20 @@ require 'open-uri'
 class Mondex::Scraper
 
   def self.scrape_list_page(index_url)
-    list_page = Nokogiri::HTML(open(index_url))
-    monsters = []
+    monsters_list = Nokogiri::HTML(open(index_url))
+    monsters = monster_list.css("#tagged-pages-container a")
 
-    list_page.css("section.jsx-1653715561.jsx-3569582989.wiki-section tr td a:last-child").each do |monster|
-      monster_link = monster.attribute("href")
-      monster_name = monster.text
-      monsters << {name: monster_name, url: monster_link}
+    monster = []
+    monsters.each do |m|
+      monster_name = m.text
+      monster_url = m.attribute("href")
+      monster << {name: monster_name, url: monster_url}
     end
-    monsters
+    monster
   end
 
-  # def self.scrape_monster_page(url)
-  #   monster = {}
-  #   monster_page = Nokogiri::HTML(open(url))
-  #
-  #
-  # end
+  def self.scrape_monster_page(url)
+
+  end
 
 end
